7d1a90345244189b85f1fa0656a9330ba065c7cd
Yang Wong <20066247+yangwong9664@users.noreply.github.com>
2023-01-12 11:03:46 +0000

    wrapper

--- app/uk/gov/hmrc/singlecustomeraccountwrapperdata/config/AppConfig.scala
+++ app/uk/gov/hmrc/singlecustomeraccountwrapperdata/config/AppConfig.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 HM Revenue & Customs
+ * Copyright 2023 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,9 +18,33 @@ package uk.gov.hmrc.singlecustomeraccountwrapperdata.config
 
 import javax.inject.{Inject, Singleton}
 import play.api.Configuration
+import uk.gov.hmrc.singlecustomeraccountwrapperdata.models.MenuItemConfig
 
 @Singleton
 class AppConfig @Inject()(config: Configuration) {
 
   val appName: String = config.get[String]("appName")
+
+  val pertaxUrl: String = s"${config.get[String]("microservice.services.pertax-frontend.url")}/personal-account"
+  val businessTaxAccountUrl: String = s"${config.get[String]("microservice.services.business-tax-frontend.url")}/business-tax-account"
+
+  private def signoutParams(continueUrl: Option[String], origin: Option[String]) = {
+    val contUrl = s"${continueUrl.fold("") { url => s"continueUrl=$url" }}"
+    val originUrl = s"${origin.fold("") { url => s"origin=$url" }}"
+    (contUrl, originUrl) match {
+      case _ if contUrl.nonEmpty && origin.nonEmpty => s"?$contUrl&$originUrl"
+      case _ if contUrl.isEmpty && origin.isEmpty => ""
+      case x@_ => s"?${x._1}${x._2}"
+    }
+  }
+
+  val menuConfig: Seq[MenuItemConfig] = Seq(
+    MenuItemConfig("Account Home", s"${pertaxUrl}", leftAligned = true, position = 0, Some("hmrc-account-icon hmrc-account-icon--home"), None),
+    MenuItemConfig("Messages", s"${pertaxUrl}/messages", leftAligned = false, position = 0, None, None),
+    MenuItemConfig("Check progress", s"${pertaxUrl}/track", leftAligned = false, position = 1, None, None),
+    MenuItemConfig("Profile and settings", s"${pertaxUrl}/profile-and-settings", leftAligned = false, position = 2, None, None),
+    MenuItemConfig("Business tax account", s"${businessTaxAccountUrl}/business-account", leftAligned = false, position = 3, None, None),
+    MenuItemConfig("Sign out", s"${pertaxUrl}/signout${signoutParams(Some("/feedback/PERTAX"), None)}", leftAligned = false, position = 4, None, None)
+  )
+
 }
