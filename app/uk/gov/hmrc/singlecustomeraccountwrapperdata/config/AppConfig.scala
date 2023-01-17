fde25535d8a52a891eba4bf0db1ce3987c2e5cd5
Yang Wong <20066247+yangwong9664@users.noreply.github.com>
2023-01-17 16:15:58 +0000

    wrapper

--- app/uk/gov/hmrc/singlecustomeraccountwrapperdata/config/AppConfig.scala
+++ app/uk/gov/hmrc/singlecustomeraccountwrapperdata/config/AppConfig.scala
@@ -21,13 +21,36 @@ import play.api.Configuration
 import uk.gov.hmrc.singlecustomeraccountwrapperdata.models.MenuItemConfig
 
 @Singleton
-class AppConfig @Inject()(config: Configuration) {
+class AppConfig @Inject()(configuration: Configuration) {
 
-  val appName: String = config.get[String]("appName")
+  val appName: String = configuration.get[String]("appName")
 
-  val pertaxUrl: String = s"${config.get[String]("microservice.services.pertax-frontend.url")}/personal-account"
-  val businessTaxAccountUrl: String = s"${config.get[String]("microservice.services.business-tax-frontend.url")}/business-tax-account"
+  private val pertaxBaseUrl: String = configuration.get[String]("sca-wrapper.pertax-frontend.base-url")
+  private val pertaxServicePath: String = configuration.get[String]("sca-wrapper.pertax-frontend.service-path")
+  private val pertaxSignoutPath: String = configuration.get[String]("sca-wrapper.pertax-frontend.signout-path")
+  val pertaxUrl = s"$pertaxBaseUrl/$pertaxServicePath"
+  private val usePertaxSignout: Boolean = configuration.get[Boolean]("sca-wrapper.signout.use-pertax-signout")
+  private val continueUrl: Option[String] = configuration.getOptional[String]("sca-wrapper.signout.continue-url")
+  private val origin: Option[String] = configuration.getOptional[String]("sca-wrapper.signout.origin")
+  val pertaxSignoutUrl = s"$pertaxUrl/$pertaxSignoutPath"
+  val customSignoutUrl: Option[String] = configuration.get[Option[String]]("sca-wrapper.signout.custom-signout-url")
 
+  private val businessTaxAccountBaseUrl: String = configuration.get[String]("sca-wrapper.business-tax-frontend.base-url")
+  val businessTaxAccountServicePath: String = configuration.get[String]("sca-wrapper.business-tax-frontend.service-path")
+  val businessTaxAccountUrl: String = s"$businessTaxAccountBaseUrl/$businessTaxAccountServicePath"
+
+
+  private def signoutUrl: String = {
+    if (usePertaxSignout) {
+      pertaxSignoutUrl
+    } else {
+      //TODO url validation
+      customSignoutUrl match {
+        case Some(url) if url.nonEmpty => customSignoutUrl.get
+        case _ => pertaxSignoutUrl
+      }
+    }
+  }
   private def signoutParams(continueUrl: Option[String], origin: Option[String]) = {
     val contUrl = s"${continueUrl.fold("") { url => s"continueUrl=$url" }}"
     val originUrl = s"${origin.fold("") { url => s"origin=$url" }}"
@@ -38,13 +61,14 @@ class AppConfig @Inject()(config: Configuration) {
     }
   }
 
+  //wrapper data handle all links
   val menuConfig: Seq[MenuItemConfig] = Seq(
     MenuItemConfig("Account Home", s"${pertaxUrl}", leftAligned = true, position = 0, Some("hmrc-account-icon hmrc-account-icon--home"), None),
     MenuItemConfig("Messages", s"${pertaxUrl}/messages", leftAligned = false, position = 0, None, None),
     MenuItemConfig("Check progress", s"${pertaxUrl}/track", leftAligned = false, position = 1, None, None),
     MenuItemConfig("Profile and settings", s"${pertaxUrl}/profile-and-settings", leftAligned = false, position = 2, None, None),
     MenuItemConfig("Business tax account", s"${businessTaxAccountUrl}/business-account", leftAligned = false, position = 3, None, None),
-    MenuItemConfig("Sign out", s"${pertaxUrl}/signout${signoutParams(Some("/feedback/PERTAX"), None)}", leftAligned = false, position = 4, None, None)
+    MenuItemConfig("Sign out", s"${signoutUrl}${signoutParams(continueUrl, origin)}", leftAligned = false, position = 4, None, None)
   )
 
 }
