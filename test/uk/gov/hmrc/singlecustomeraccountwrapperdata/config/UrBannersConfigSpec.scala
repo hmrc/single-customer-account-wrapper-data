2fd87c6ac75bd74a0a66bfd113559744065e9295
askar.makkunnath <askar.makkunnath@digital.hmrc.gov.uk>
2025-08-06 09:13:43 +0100

    DDCNL-10887: Updating spec

--- test/uk/gov/hmrc/singlecustomeraccountwrapperdata/config/UrBannersConfigSpec.scala
+++ test/uk/gov/hmrc/singlecustomeraccountwrapperdata/config/UrBannersConfigSpec.scala
@@ -24,23 +24,23 @@ import uk.gov.hmrc.singlecustomeraccountwrapperdata.fixtures.BaseSpec
 class UrBannersConfigSpec extends BaseSpec {
 
   lazy val testBanner1: UrBanner = UrBanner("/home", "TestLink1", true)
-  lazy val testBanner2: UrBanner = UrBanner("/another-page", "TestLink2", false)
-  lazy val testBanner3: UrBanner = UrBanner("/second-service", "TestLink3", true)
+  lazy val testBanner2: UrBanner = UrBanner("/first-page", "TestLink2", false)
+  lazy val testBanner3: UrBanner = UrBanner("/second-page", "TestLink3", true)
 
   override implicit lazy val app: Application =
     GuiceApplicationBuilder()
       .configure(
-        "ur-banners.0.service"     -> "test-frontend",
-        "ur-banners.0.0.page"      -> testBanner1.page,
-        "ur-banners.0.0.link"      -> testBanner1.link,
-        "ur-banners.0.0.isEnabled" -> testBanner1.isEnabled,
-        "ur-banners.0.1.page"      -> testBanner2.page,
-        "ur-banners.0.1.link"      -> testBanner2.link,
-        "ur-banners.0.1.isEnabled" -> testBanner2.isEnabled,
-        "ur-banners.1.service"     -> "second-frontend",
-        "ur-banners.1.0.page"      -> testBanner3.page,
-        "ur-banners.1.0.link"      -> testBanner3.link,
-        "ur-banners.1.0.isEnabled" -> testBanner3.isEnabled
+        "ur-banners.items.0.service"             -> "test-frontend-1",
+        "ur-banners.items.0.entries.0.page"      -> testBanner1.page,
+        "ur-banners.items.0.entries.0.link"      -> testBanner1.link,
+        "ur-banners.items.0.entries.0.isEnabled" -> testBanner1.isEnabled,
+        "ur-banners.items.0.entries.1.page"      -> testBanner2.page,
+        "ur-banners.items.0.entries.1.link"      -> testBanner2.link,
+        "ur-banners.items.0.entries.1.isEnabled" -> testBanner2.isEnabled,
+        "ur-banners.items.1.service"             -> "test-frontend-2",
+        "ur-banners.items.1.entries.0.page"      -> testBanner3.page,
+        "ur-banners.items.1.entries.0.link"      -> testBanner3.link,
+        "ur-banners.items.1.entries.0.isEnabled" -> testBanner3.isEnabled
       )
       .build()
 
@@ -48,11 +48,20 @@ class UrBannersConfigSpec extends BaseSpec {
 
   "UrBannersConfig" must {
     "return a list of banners for all services" in {
-      bannersConfig.getUrBannersByService must be
-      Map(
-        "test-frontend"                     -> List(testBanner1, testBanner2),
-        "second-frontend"                   -> List(testBanner3)
-      )
+      bannersConfig.getUrBannersByService mustBe
+        Map(
+          "test-frontend-1" -> List(testBanner1, testBanner2),
+          "test-frontend-2" -> List(testBanner3)
+        )
+    }
+
+    "return empty map when no items are present" in {
+      val appWithEmptyConfig = GuiceApplicationBuilder()
+        .configure("ur-banners.items" -> List.empty)
+        .build()
+
+      val emptyConfig = appWithEmptyConfig.injector.instanceOf[UrBannersConfig]
+      emptyConfig.getUrBannersByService mustBe Map.empty
     }
   }
 
