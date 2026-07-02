774a53f5903eb7b8e2a0bf014917950131a0962f
askar.makkunnath <askar.makkunnath@digital.hmrc.gov.uk>
2026-07-02 11:29:58 +0100

    DDCNL-12087: Cleanup

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,18 +3,18 @@ import sbt.*
 object AppDependencies {
 
   private val playVersion = "play-30"
-  private val bootstrapVersion = "10.7.0"
+  private val bootstrapVersion = "10.7.1"
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc" %% s"bootstrap-backend-$playVersion" % bootstrapVersion,
-    "uk.gov.hmrc" %% s"domain-$playVersion" % "13.0.0",
-    "uk.gov.hmrc" %% s"play-partials-$playVersion" % "10.2.0",
-    "org.typelevel" %% "cats-core" % "2.13.0",
-    "uk.gov.hmrc" %% s"mongo-feature-toggles-client-$playVersion" % "2.5.0"
+    "uk.gov.hmrc"   %% s"bootstrap-backend-$playVersion"            % bootstrapVersion,
+    "uk.gov.hmrc"   %% s"domain-$playVersion"                       % "13.0.0",
+    "uk.gov.hmrc"   %% s"play-partials-$playVersion"                % "10.2.0",
+    "org.typelevel" %% "cats-core"                                  % "2.13.0",
+    "uk.gov.hmrc"   %% s"mongo-feature-toggles-client-$playVersion" % "2.5.0"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% s"bootstrap-test-$playVersion" % bootstrapVersion,
-    "uk.gov.hmrc" %% s"domain-test-$playVersion" % "13.0.0"
+    "uk.gov.hmrc" %% s"domain-test-$playVersion"    % "13.0.0"
   ).map(_ % Test)
 }
